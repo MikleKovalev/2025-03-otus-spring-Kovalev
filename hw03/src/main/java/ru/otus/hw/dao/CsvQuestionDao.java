b205c80345ecdf50ca66c21169ef8c827912004f
MikleKovalev <mikle.kovalev@gmail.com>
2025-05-04 12:32:18 +0400

    Add code from hw02 and setup localization

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,25 +1,46 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
-@Component
 public class CsvQuestionDao implements QuestionDao {
+    private static final int BUFFER_SIZE = 32768;
+
+    private static final int SKIP_LINES_IN_CSV = 1;
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
-    public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+    public List<Question> findAll() throws QuestionReadException {
+        try (var inputStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream(fileNameProvider.getTestFileName());
+             var reader = new BufferedReader(
+                     new InputStreamReader(inputStream, StandardCharsets.UTF_8),
+                     BUFFER_SIZE)) {
+            return new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSkipLines(SKIP_LINES_IN_CSV)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .build()
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (Exception e) {
+            throw new QuestionReadException(e.getMessage());
+        }
     }
 }
--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,12 +3,15 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
 @Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
+
+    private static final int MIN_ANSWER_NUMBER = 1;
 
     private final LocalizedIOService ioService;
 
@@ -24,10 +27,41 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+            runQuestionAttempt(testResult, question);
         }
         return testResult;
     }
 
+    private void runQuestionAttempt(TestResult testResult, Question question) {
+        var isAnswerValid = false;
+        try {
+            askQuestion(question);
+            int answerNumber = getAnswerNumber(question);
+            isAnswerValid = question.answers().get(answerNumber).isCorrect();
+        } catch (Exception e) {
+            ioService.printLineLocalized("TestService.failed.attempt");
+        }
+        testResult.applyAnswer(question, isAnswerValid);
+    }
+
+    private void askQuestion(Question question) {
+        ioService.printLine(question.text());
+        for (int i = 0; i < question.answers().size(); ++i) {
+            ioService.printFormattedLine(
+                    "%d) %s",
+                    i + MIN_ANSWER_NUMBER,
+                    question.answers().get(i).text());
+        }
+        ioService.printLineLocalized("TestService.what.is.number");
+    }
+
+    private int getAnswerNumber(Question question) {
+        int lastAnswerNumber = question.answers().size();
+        var answerNumber = ioService.readIntForRangeLocalized(
+                MIN_ANSWER_NUMBER,
+                lastAnswerNumber,
+                "TestService.wrong.answer.number");
+        answerNumber -= MIN_ANSWER_NUMBER;
+        return answerNumber;
+    }
 }
